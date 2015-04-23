158be1fcbb294a9d09f666c0d48b2952b68ae8f7
Guillaume Tournier <gtournier@ventardis.fr>
2015-04-23 15:48:25 +0200

    Memo options

--- src/mediator.js
+++ src/mediator.js
@@ -1,27 +1,34 @@
-
 var Mediator = (function() {
   "use strict";
 
+  var toString       = ({}).toString,
+      hasOwnProperty = ({}).hasOwnProperty;
+
   return {
     events: {},
+    defaultMemo: {},
+    memoAsSecondArg: false,
 
     // Observe one or multiple eventNames.
     // eventNames: a list of space-separated event names
-    // [func1, func2...]: a list of functions
+    // [func1, func2...]: a list of handlers
     on: function(eventNames) {
       var argLen = arguments.length;
       if (!eventNames || argLen < 2) {
         return this;
       }
       var events = this.events,
-          args = arguments;
+          args   = arguments;
       forEachEventName(eventNames, function(eventName) {
         if (!events[eventName]) {
           events[eventName] = [];
         }
-        var list = events[eventName];
+        var list = events[eventName], arg;
         for (var i = 1; i < argLen; ++i) {
-          list.push(args[i]);
+          arg = args[i];
+          if ('function' === typeof arg) {
+            list.push(arg);
+          }
         }
       });
       return this;
@@ -38,7 +45,7 @@ var Mediator = (function() {
         return this;
       }
       var events = this.events,
-          args = arguments;
+          args   = arguments;
       if (1 === argLen) {
         forEachEventName(eventNames, function(eventName) {
           delete events[eventName];
@@ -46,10 +53,11 @@ var Mediator = (function() {
       } else {
         forEachEventName(eventNames, function(eventName) {
           var list = events[eventName] || [], j;
-          for (var i = list.length - 1; i >= 0; --i) {
-            for (j = 1; j < argLen; ++j) {
-              if (args[j] === list[i]) {
-                list.splice(i, 1);
+          for (var i = 1; i < argLen; ++i) {
+            j = list.length;
+            while(j--) {
+              if (args[i] === list[j]) {
+                list.splice(j, 1);
               }
             }
           }
@@ -61,16 +69,34 @@ var Mediator = (function() {
       return this;
     },
 
-    // Call all "eventName" events. Give memo as first argument
+    // Call all "eventName" handlers. Give memo in argument.
     trigger: function(eventNames, memo) {
       if (!eventNames) {
         return this;
       }
-      var events = this.events;
+      var defaultMemo = this.defaultMemo;
+      if (arguments.length < 2) {
+        memo = defaultMemo;
+      } else {
+        if ('[object Object]' === toString.call(memo) &&
+            '[object Object]' === toString.call(defaultMemo)) {
+          for (var prop in defaultMemo) {
+            if (!hasOwnProperty.call(memo, prop)) {
+              memo[prop] = defaultMemo[prop];
+            }
+          }
+        }
+      }
+      var events = this.events,
+          memoAsSecondArg = this.memoAsSecondArg;
       forEachEventName(eventNames, function(eventName) {
         var list = events[eventName] || [];
         for (var i = 0, len = list.length; i < len; ++i) {
-          list[i](memo);
+          if (memoAsSecondArg) {
+            list[i](null, memo);
+          } else {
+            list[i](memo);
+          }
         }
       });
       return this;
--- src/test/tests.js
+++ src/test/tests.js
@@ -3,6 +3,8 @@ var bar = [],
     foo2 = function() { bar.push('2'); },
     foo3 = function() { bar.push('3'); },
     foo4 = function() { bar.push('4'); };
+    foo5 = function(memo) { bar.push(memo); },
+    foo6 = function(__, memo) { bar.push(memo); }
 
 QUnit.module("Cleanup", {
   afterEach: function() {
@@ -19,7 +21,7 @@ QUnit.test("Simple event, simple callback", function(assert) {
 
   Mediator.trigger('foo1');
   assert.equal(bar.length, 1, "Callback called");
-  assert.equal(bar[0], '1', "Callback value");
+  assert.equal(bar[0], '1', "Event handler value");
   
   Mediator.off('foo1');
   objectKeys = Object.keys(Mediator.events);
@@ -27,7 +29,7 @@ QUnit.test("Simple event, simple callback", function(assert) {
   assert.equal(objectKeys.length, 0, "Event names length ok");
   
   Mediator.trigger('foo1');
-  assert.equal(bar.length, 1, "Callback not called");
+  assert.equal(bar.length, 1, "Event handler not called");
   
   Mediator.on('foo1', foo2);
   var objectKeys = Object.keys(Mediator.events);
@@ -35,8 +37,8 @@ QUnit.test("Simple event, simple callback", function(assert) {
   assert.equal(objectKeys.length, 1, "Event names length ok");
   
   Mediator.trigger('foo1');
-  assert.equal(bar.length, 2, "Callback called");
-  assert.equal(bar[1], '2', "Callback value");
+  assert.equal(bar.length, 2, "Event handler called");
+  assert.equal(bar[1], '2', "Event handler value");
   
   Mediator.off();
   objectKeys = Object.keys(Mediator.events);
@@ -44,10 +46,10 @@ QUnit.test("Simple event, simple callback", function(assert) {
   assert.equal(objectKeys.length, 0, "Event names length ok");
   
   Mediator.trigger('foo1');
-  assert.equal(bar.length, 2, "Callback not called");
+  assert.equal(bar.length, 2, "Event handler not called");
 });
 
-QUnit.test("No callback", function(assert) {
+QUnit.test("No Event handler", function(assert) {
   Mediator.on('foo1').on('foo1 foo2 foo3');
   var objectKeys = Object.keys(Mediator.events);
   assert.notOk('foo1' in Mediator.events, "Event name value not present");
@@ -64,8 +66,8 @@ QUnit.test("Multiple event names", function(assert) {
   assert.equal(objectKeys.length, 3, "Event names length ok");
   
   Mediator.trigger('foo1 foo2 foo3');
-  assert.equal(bar.length, 3, "Callback called");
-  assert.deepEqual(bar, ['1', '1', '1'], "Callback values");
+  assert.equal(bar.length, 3, "Event handler called");
+  assert.deepEqual(bar, ['1', '1', '1'], "Event handler values");
   
   Mediator.off('foo1 foo2 foo3');
   objectKeys = Object.keys(Mediator.events);
@@ -76,63 +78,96 @@ QUnit.test("Multiple event names", function(assert) {
   assert.equal(objectKeys.length, 0, "Event names length ok");
   
   Mediator.trigger('foo1 foo2 foo3').trigger('foo1').trigger('foo2').trigger('foo3');
-  assert.equal(bar.length, 3, "Callback not called");
+  assert.equal(bar.length, 3, "Event handler not called");
 });
 
-QUnit.test("Multiple callbacks", function(assert) {
+QUnit.test("Multiple Event handlers", function(assert) {
   Mediator.on('foo1', foo1, foo2, foo3);
   var objectKeys = Object.keys(Mediator.events);
   assert.equal(objectKeys.length, 1, "Event names length ok");
   
   Mediator.trigger('foo1');
-  assert.equal(bar.length, 3, "Callback called");
-  assert.deepEqual(bar, ['1', '2', '3'], "Callback values");
+  assert.equal(bar.length, 3, "Event handler called");
+  assert.deepEqual(bar, ['1', '2', '3'], "Event handler values");
   
   Mediator.off('foo1', foo1);
-  assert.equal(Mediator.events['foo1'].length, 2, "Callbacks length ok");
+  assert.equal(Mediator.events['foo1'].length, 2, "Event handlers length ok");
   
   Mediator.trigger('foo1');
-  assert.equal(bar.length, 5, "Callback called");
-  assert.deepEqual(bar, ['1', '2', '3', '2', '3'], "Callback values");
+  assert.equal(bar.length, 5, "Event handler called");
+  assert.deepEqual(bar, ['1', '2', '3', '2', '3'], "Event handler values");
   
   Mediator.off('foo1', foo1, foo2, foo3);
   assert.notOk('foo1' in Mediator, "Event names not in events");
   
   Mediator.trigger('foo1');
-  assert.equal(bar.length, 5, "Callback not called");
+  assert.equal(bar.length, 5, "Event handler not called");
 });
 
-QUnit.test("Multiple events & multiple callbacks", function(assert) {
+QUnit.test("Multiple events & multiple Event handlers", function(assert) {
   Mediator.on('foo1 foo2', foo1, foo2);
   var objectKeys = Object.keys(Mediator.events);
   assert.equal(objectKeys.length, 2, "Event names length ok");
   
   Mediator.trigger('foo1');
-  assert.equal(bar.length, 2, "Callback called");
-  assert.deepEqual(bar, ['1', '2'], "Callback values");
+  assert.equal(bar.length, 2, "Event handler called");
+  assert.deepEqual(bar, ['1', '2'], "Event handler values");
   Mediator.trigger('foo2 foo2');
-  assert.equal(bar.length, 6, "Callback called");
-  assert.deepEqual(bar, ['1', '2', '1', '2', '1', '2'], "Callback values");
+  assert.equal(bar.length, 6, "Event handler called");
+  assert.deepEqual(bar, ['1', '2', '1', '2', '1', '2'], "Event handler values");
   
   Mediator.off('foo1', foo1);
-  assert.equal(Mediator.events['foo1'].length, 1, "Callbacks length ok");
-  assert.equal(Mediator.events['foo2'].length, 2, "Callbacks length ok");
+  assert.equal(Mediator.events['foo1'].length, 1, "Event handlers length ok");
+  assert.equal(Mediator.events['foo2'].length, 2, "Event handlers length ok");
   
   Mediator.trigger('foo1');
-  assert.equal(bar.length, 7, "Callback called");
-  assert.equal(bar[6], '2', "Callback values");
+  assert.equal(bar.length, 7, "Event handler called");
+  assert.equal(bar[6], '2', "Event handler values");
   
   Mediator.trigger('foo2');
-  assert.equal(bar.length, 9, "Callback called");
-  assert.deepEqual(bar, ['1', '2', '1', '2', '1', '2', '2', '1', '2'], "Callback values");
+  assert.equal(bar.length, 9, "Event handler called");
+  assert.deepEqual(bar, ['1', '2', '1', '2', '1', '2', '2', '1', '2'], "Event handler values");
   
   Mediator.off('foo2', foo1, foo2);
   assert.notOk('foo2' in Mediator, "Event names not in events");
   
   Mediator.trigger('foo2');
-  assert.equal(bar.length, 9, "Callback not called");
+  assert.equal(bar.length, 9, "Event handler not called");
   
   Mediator.trigger('foo1');
-  assert.equal(bar.length, 10, "Callback called");
-  assert.equal(bar[9], '2', "Callback values");
+  assert.equal(bar.length, 10, "Event handler called");
+  assert.equal(bar[9], '2', "Event handler values");
+});
+
+QUnit.test("Memo", function(assert) {
+  Mediator.on('foo5', foo5).trigger('foo5', { foo: 'FOO' });
+  assert.deepEqual(bar[0], { foo: 'FOO' }, "Memo value");
+  
+  Mediator.memoAsSecondArg = true;
+  
+  Mediator.trigger('foo5', { foo: 'FOO' });
+  assert.equal(bar[1], null, "Memo value");
+  Mediator.on('foo6', foo6).trigger('foo6', { foo: 'FOO' });
+  assert.deepEqual(bar[2], { foo: 'FOO' }, "Memo value");
+  
+  Mediator.defaultMemo = 5;
+  
+  Mediator.trigger('foo6');
+  assert.equal(bar[3], 5, "Memo value");
+  
+  Mediator.defaultMemo = { foo: 'FOO' };
+  
+  Mediator.trigger('foo6', { bar: 'BAR' });
+  assert.deepEqual(bar[4], { foo: 'FOO', bar: 'BAR' }, "Memo value");
+  Mediator.trigger('foo6', { bar: 'BAR', foo: null });
+  assert.deepEqual(bar[5], { foo: null, bar: 'BAR' }, "Memo value");
+  Mediator.trigger('foo6', { bar: 'BAR', foo: void(0) });
+  assert.deepEqual(bar[6], { foo: void(0), bar: 'BAR' }, "Memo value");
+  
+  Mediator.defaultMemo = null;
+  Mediator.trigger('foo6');
+  assert.ok(bar[7] === null, "Memo value");
+  Mediator.defaultMemo = void 0;
+  Mediator.trigger('foo6');
+  assert.ok(bar[8] === void 0, "Memo value");
 });
